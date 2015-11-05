cddd18914859dbb68de266cc21fead4db01615ff
DanBuddy <ds321609@ohio.edu>
2015-11-04 21:03:51 -0500

    working timer using Date()

--- timeScript.js
+++ timeScript.js
@@ -1,29 +1,61 @@
-var milliseconds = 120 * 1000;
-var timeDiv = document.getElementById('time-area');
-var running = setInterval(timer, 1000);
+var inputMin = 1,
+    currentTime = new Date().getTime(),
+    endTime = new Date(),
+    timeDiv = document.getElementById('time-area'),
+    running;
 
-function timer() {
-  var minAmt = timeConversion(milliseconds).minutes;
-  var secAmt = timeConversion(milliseconds).seconds;
-  timeDiv.innerHTML = minAmt + ':' + secAmt;
-  console.log(timeDiv.innerHTML = minAmt + ':' + secAmt);
-  if (minAmt < 10) {
-    timeDiv.innerHTML = '0' + minAmt + ":" + secAmt;
+//UI functions
+
+function render(min, sec) {
+
+  timeDiv.innerHTML = min + ':' + sec;
+
+  if(min < 10) {
+    timeDiv.innerHTML = '0' + min + ":" + sec;
   }
-  if (milliseconds <= 0) {
-    secAmt = 0;
-    clearInterval(running);
+  if(sec < 10) {
+    timeDiv.innerHtml = '0' + min + ":" + sec;
 
-
+    console.log(timeDiv.innerHTML = min + ':' + sec);
   }
-
-  milliseconds -= 1000;
 }
+
+function update() {
+  currentTime = new Date().getTime();
+
+  var minAmt = timeConversion(currentTime).minutes;
+  var secAmt = timeConversion(currentTime).seconds;
+
+  console.log(minAmt + secAmt);
+  render(minAmt, secAmt);
+
+
+  if(minAmt <=0 && secAmt <=0) {
+    clearInterval(running);
+  }
+
+}
+
+//Calculation functions
 
 function timeConversion(rawTime) {
+  var length = (endTime - rawTime);
   return {
-      minutes: Math.floor(milliseconds/(60000)),
-      seconds: Math.floor(milliseconds/1000)
+      minutes: Math.floor(length/600000),
+      seconds: Math.floor((length/1000) % 60)
   }
 
 }
+
+
+function startTimer() {
+  endTime.setMinutes(endTime.getMinutes() + inputMin);
+
+  running = setInterval(update, 1000);
+
+}
+
+startTimer();
+
+//Helper functions
+  //Break timer function which runs after the "work" timer function   finishes
